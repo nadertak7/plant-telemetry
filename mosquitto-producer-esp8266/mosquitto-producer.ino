6aa7353f5912772507e17c3a937dfa0ee6c97470
Nader Tak <nadertak7@gmail.com>
2025-06-22 03:07:04 +0100

    Mosquitto producer

---
+++ mosquitto-producer-esp8266/mosquitto-producer.ino
@@ -0,0 +1,97 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "wifi_secrets.h"
+
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+
+// Sensor settings
+const int MOISTURE_SENSOR_PIN = A0;
+const int DRY_VALUE = 666;
+const int WET_VALUE = 272;
+const int READ_DELAY_MS = 2000;
+
+// Wifi Secrets from wifi_secrets.h
+const char* WIFI_SSID_VALUE = WIFI_SSID;
+const char* WIFI_PASS_VALUE = WIFI_PASS;
+
+// MQTT settings
+const char* MQTT_BROKER_IP_VALUE = MQTT_BROKER_IP;
+const char* MQTT_USERNAME_VALUE = MQTT_USERNAME;
+const char* MQTT_PASSWORD_VALUE = MQTT_PASSWORD;
+const char* MQTT_CLIENT_ID_VALUE = "ESP8266_Sensor_01";
+const char* MQTT_PUBLISH_TOPIC = "livingroom/plant1";
+
+// Time settings
+const int WIFI_RETRY_DELAY = 5000;
+const int MQTT_RETRY_DELAY = 5000;
+const int SEND_INTERVAL = 5000;
+unsigned long lastSendTime = 0;
+
+void connect_wifi() {
+  Serial.println();
+  Serial.print("Connecting to Wifi network ");
+  Serial.println(WIFI_SSID_VALUE);
+
+  WiFi.begin(WIFI_SSID_VALUE, WIFI_PASS_VALUE);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("WiFi connected...");
+}
+
+void connect_mqtt() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect(MQTT_CLIENT_ID_VALUE, MQTT_USERNAME_VALUE, MQTT_PASSWORD_VALUE)) {
+      Serial.println("Connected to MQTT Broker...");
+    } else {
+      Serial.print("MQTT connection failed. Error code = ");
+      Serial.println(client.state());
+      Serial.println("Retrying...");
+      delay(MQTT_RETRY_DELAY);
+    }
+  }
+}
+
+String get_moisture_reading() {
+  // Take sensor reading
+  int rawMoistureValue = analogRead(MOISTURE_SENSOR_PIN);
+  int moisturePercentage = map(rawMoistureValue, DRY_VALUE, WET_VALUE, 0, 100);
+  // In case moisture percentage falls outside of 0-100 range
+  moisturePercentage = constrain(moisturePercentage, 0, 100);
+
+  // Compile into json
+  String moistureData = "{";
+  moistureData += "\"raw\":" + String(rawMoistureValue) + ",";
+  moistureData += "\"percentage\":" + String(moisturePercentage);
+  moistureData += "}";
+
+  return moistureData;
+}
+
+void setup() {
+  while (!Serial) {
+    ;
+  }
+  connect_wifi();
+  client.setServer(MQTT_BROKER_IP_VALUE, 1883);
+  connect_mqtt();
+}
+
+void loop() {
+  // In case mqtt connection is no longer active
+  if (!client.connected()) {
+    connect_mqtt();
+  }
+
+  unsigned long now = millis();
+  if (now - lastSendTime > SEND_INTERVAL) {
+    lastSendTime = now;
+  }
+
+  String payload = get_moisture_reading();
+  client.publish(MQTT_PUBLISH_TOPIC, payload.c_str(), true, 1);  // 'true' arg ensures message retention
+}
