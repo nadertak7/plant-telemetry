085aa8810528eba3e01cd96ef88d9e7ed6b2cd91
Nader Tak <nadertak7@gmail.com>
2025-07-03 01:30:43 +0100

    Map topic enum to id (for db)

--- mosquitto-consumer/src/mosquitto_consumer/config/enums.py
+++ mosquitto-consumer/src/mosquitto_consumer/config/enums.py
@@ -1,8 +1,28 @@
-from enum import StrEnum
+from enum import auto, Enum, StrEnum
+from dataclasses import dataclass
 
 
-class MosquittoTopics(StrEnum):
+class MosquittoTopics(Enum):
     """String enums for MQTT topics to subscribe to."""
 
-    SCARLET_STAR = "plant-monitoring/living-room/scarlet-star-1/telemetry"
-    # More TBD
+    @dataclass(frozen=True)
+    class TopicIdMapping:
+        id: int
+        topic: str
+
+    SCARLET_STAR = TopicIdMapping(1, "plant-monitoring/living-room/scarlet-star-1/telemetry")
+    # Add more topics here when ESP modules are connected to new/different plants
+
+    @property
+    def id(self):
+        return self.value.id
+
+    @property
+    def topic(self):
+        return self.value.topic
+
+class TableNames(StrEnum):
+    """String enums for Postgres table names."""
+
+    PLANTS = auto()
+    PLANTS_MOISTURE_LOG = auto()
