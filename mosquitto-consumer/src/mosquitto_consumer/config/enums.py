e1f48a098e1a74e323c0a249021ad953332aa276
Nader Tak <nadertak7@gmail.com>
2025-07-05 02:42:25 +0100

    Add typehints and docstrings to enum file

--- mosquitto-consumer/src/mosquitto_consumer/config/enums.py
+++ mosquitto-consumer/src/mosquitto_consumer/config/enums.py
@@ -1,28 +1,36 @@
-from enum import auto, Enum, StrEnum
 from dataclasses import dataclass
+from enum import Enum, StrEnum, auto
 
+
+@dataclass(frozen=True)
+class TopicIdMapping:
+    """Maps an incrementing ID to a topic."""
+
+    id: int
+    topic: str
 
 class MosquittoTopics(Enum):
-    """String enums for MQTT topics to subscribe to."""
+    """Enumerate MQTT topics."""
 
-    @dataclass(frozen=True)
-    class TopicIdMapping:
-        id: int
-        topic: str
+    _value_: TopicIdMapping
 
     SCARLET_STAR = TopicIdMapping(1, "plant-monitoring/living-room/scarlet-star-1/telemetry")
     # Add more topics here when ESP modules are connected to new/different plants
 
     @property
-    def id(self):
+    def id(self) -> int:
+        """Property for id. Use like `MosquittoTopics.SCARLET_STAR.id`."""
         return self.value.id
 
     @property
-    def topic(self):
+    def topic(self) -> str:
+        """Property for topic. Use like `MosquittoTopics.SCARLET_STAR.topic`."""
         return self.value.topic
 
 class TableNames(StrEnum):
     """String enums for Postgres table names."""
+
+    _value_: auto
 
     PLANTS = auto()
     PLANTS_MOISTURE_LOG = auto()
