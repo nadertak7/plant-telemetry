4fe888112c0c85de90c7f7946661b93ecb534590
Nader Tak <nadertak7@gmail.com>
2025-07-06 02:51:25 +0100

    Add parent and child exceptions for sql and mqtt

--- mosquitto-consumer/src/mosquitto_consumer/utils/exceptions.py
+++ mosquitto-consumer/src/mosquitto_consumer/utils/exceptions.py
@@ -1,15 +1,39 @@
-class DialectDriverError(Exception):
+# SqlClient errors
+class SqlClientError(Exception):
+    """Inhert by all exceptions raised by SqlClient.
+
+    Use to catch any database error when using the SqlClient, especially as a
+        context manager.
+
+    Usage:
+        try:
+            with sql_client.get_session() as session, session.begin():
+                client.create_schema()
+                client.execute_sql("select 0 from table")
+        except SqlClientError as exception:
+            logging.exception("Database operation failure.")
+            raise SqlClientError() from exception
+    """
+
+    pass
+
+class DialectDriverError(SqlClientError):
     """Raise when SQLAlchemy dialect or driver is not found in environment or invalid."""
 
     pass
 
-class DatabaseConnectionError(Exception):
-    """Raise when SQLAlchemy raises an OperationalError when connecting to database."""
+class DatabaseConnectionError(SqlClientError):
+    """Raise when SQLAlchemy encounters an error when connecting to database."""
 
     pass
 
-class SqlQueryError(Exception):
-    """Raise when there is an error while executing a query via SQLAlchemy."""
+class SchemaCreationError(SqlClientError):
+    """Raise when SQLAlchemy encounters an error while creating from a specified model."""
+
+    pass
+
+class SqlQueryError(SqlClientError):
+    """Raise when SQLAlchemy encounters an error while executing a query."""
 
     def __init__(self, query: str) -> None:
         """Generate the message and call base class constructor.
@@ -20,3 +44,24 @@ class SqlQueryError(Exception):
         """
         self.query = query
         super().__init__(f"Error while executing query: {self.query}")
+
+# Mqtt consumer errors
+class MqttConsumerError(Exception):
+    """Inherit by all exceptions raised by mqtt consumer."""
+
+    pass
+
+class JsonPayloadDecodeError(MqttConsumerError):
+    """Raise when an error is encountered while processing the JSON payload from the MQTT broker."""
+
+    pass
+
+class ModelObjectProcessingError(MqttConsumerError):
+    """Raise when an error is encountered while creating a SQLAlchemy model object with data."""
+
+    pass
+
+class MqttBrokerConnectionError(MqttConsumerError):
+    """Raise when mosquitto encounters an error while connecting to the specified client."""
+
+    pass
