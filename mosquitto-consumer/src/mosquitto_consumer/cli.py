a47fe639de8a5587bd7588048e0c14c847a21278
Nader Tak <nadertak7@gmail.com>
2025-07-08 23:59:27 +0100

    Enable add_plants function to be called via cli

---
+++ mosquitto-consumer/src/mosquitto_consumer/cli.py
@@ -0,0 +1,25 @@
+import click
+
+from mosquitto_consumer.utils.add_plants import add_plants
+
+# ^ Though sql_client is also imported into mqtt_consumer_client.py,
+# the SQLAlchemy engine is not created twice due to the way python
+# caches modules.
+
+@click.group
+def cli() -> None:
+    """Command line interface for Mosquitto consumer."""
+    pass
+
+@cli.command
+def updateplants() -> None:
+    """Update the plants table if new items have been added while the script is running."""
+    add_plants
+
+if __name__ == "__main__":
+    cli()
+
+
+
+
+
