b5df0145b90a7f55a563c9f6671916dbb01cef96
Nader Tak <nadertak7@gmail.com>
2025-10-02 22:40:48 +0100

    Implement means to deprecate and de-deprecate plants in cli

--- mosquitto-consumer/src/mosquitto_consumer/cli.py
+++ mosquitto-consumer/src/mosquitto_consumer/cli.py
@@ -1,10 +1,14 @@
-import click
+from typing import Optional
 
+import click
+from sqlalchemy.exc import SQLAlchemyError
+
+from mosquitto_consumer.config.exceptions import SqlClientError
+from mosquitto_consumer.config.logs import logger
+from mosquitto_consumer.database.models import Plant
+from mosquitto_consumer.database.sql_client import sql_client
 from mosquitto_consumer.utils.plants_utils import add_plant
 
-# ^ Though sql_client is also imported into mqtt_consumer_client.py,
-# the SQLAlchemy engine is not created twice due to the way python
-# caches modules.
 
 @click.group
 def cli() -> None:
@@ -43,6 +47,38 @@ def addplant(plant_name: str, topic_plant_name: str, topic_plant_location: str) 
         click.echo("Successfully added plant.")
         click.secho("Warning: Container must be restarted to receive messages from this topic.", fg="yellow")
 
+@cli.command
+@click.option(
+    "--plant_id",
+    prompt="Enter the id of the plant in the plants table which you would like to set the deprecation status of.",
+    help="The id of the plant to be deprecated or de-deprecated."
+)
+@click.option(
+    "--is_deprecated",
+    is_flag=True,
+    default=True,
+    prompt="Enter the desired deprecation status of the plant.",
+    help="The desired deprecation status of the plant."
+)
+def deprecate(plant_id: int, is_deprecated: bool) -> None:
+    """Deprecate or de-deprecate a plant_id via cli."""
+    try:
+        with sql_client.get_session() as session, session.begin():
+            selected_plant: Optional[Plant] = session.get(Plant, plant_id)
+            if not selected_plant:
+                click.secho(f"Error: Plant ID {plant_id} does not exist. Try again.", fg="red")
+                return
+
+            click.echo(f"Setting deprecation status to {is_deprecated} for plant {selected_plant.plant_name}")
+            click.confirm("Do you want to continue?", abort=True)
+            click.echo("Adding plant to database.")
+            selected_plant.is_deprecated = is_deprecated
+    except SqlClientError:
+        logger.exception("Error while retrieving topics from plants table.")
+        raise
+    except SQLAlchemyError:
+        logger.exception("Unexpected error while retrieving topics from plants table.")
+        raise
 
 if __name__ == "__main__":
     cli()
