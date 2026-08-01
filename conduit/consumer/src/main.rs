51d45cd53a08f952fde400ac4cf20c2d8735312a
Nader Tak <nadertak7@gmail.com>
2026-08-01 01:37:36 +0100

    feat: add mqtt client connection

--- conduit/consumer/src/main.rs
+++ conduit/consumer/src/main.rs
@@ -2,6 +2,8 @@ use shared::db;
 use shared::logger;
 use shared::settings::Settings;
 
+mod mqtt;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenvy::dotenv().ok();
@@ -9,6 +11,8 @@ async fn main() -> anyhow::Result<()> {
     let settings = Settings::new()?;
     let pool = db::create_connection_pool(&settings.database_settings).await?;
     db::run_migrations(&pool).await?;
+    let (mqtt_client, _event_loop) = mqtt::get_client(&settings.mqtt_settings);
+    mqtt::subscribe(&mqtt_client, &settings.mqtt_settings).await?;
     println!("Got to this point.");
     Ok(())
 }
--- conduit/shared/src/settings.rs
+++ conduit/shared/src/settings.rs
@@ -1,6 +1,7 @@
 use anyhow::Context;
+use rumqttc::{MqttOptions, QoS};
 use sqlx::postgres::PgConnectOptions;
-use std::env;
+use std::{env, time::Duration};
 
 pub struct DatabaseSettings {
     database_url: String,
@@ -15,8 +16,31 @@ impl DatabaseSettings {
     }
 }
 
+pub struct MqttSettings {
+    id: String,
+    host: String,
+    port: u16,
+    username: String,
+    password: String,
+    keep_alive_seconds: u64,
+    pub subscribe_topic: String,
+    pub request_queue_capacity: usize,
+    pub quality_of_service: QoS,
+}
+
+impl MqttSettings {
+    pub fn connect_options(&self) -> MqttOptions {
+        let mut mqtt_options = MqttOptions::new(&self.id, &self.host, self.port);
+        mqtt_options
+            .set_credentials(&self.username, &self.password)
+            .set_keep_alive(Duration::from_secs(self.keep_alive_seconds));
+        mqtt_options
+    }
+}
+
 pub struct Settings {
     pub database_settings: DatabaseSettings,
+    pub mqtt_settings: MqttSettings,
 }
 
 impl Settings {
@@ -26,6 +50,19 @@ impl Settings {
                 database_url: env::var("DATABASE_URL")
                     .context("Could not find DATABASE_URL in environment.")?,
                 max_connections: 5,
+            },
+            mqtt_settings: MqttSettings {
+                id: "conduit-consumer".to_string(),
+                host: "localhost".to_string(),
+                port: 1883,
+                username: env::var("MQTT_USERNAME")
+                    .context("Could not find MQTT_USERNAME in environment.")?,
+                password: env::var("MQTT_PASSWORD")
+                    .context("Could not find MQTT_PASSWORD in environment.")?,
+                subscribe_topic: "sensor/+".to_string(),
+                keep_alive_seconds: 60,
+                request_queue_capacity: 10,
+                quality_of_service: QoS::AtMostOnce,
             },
         })
     }
