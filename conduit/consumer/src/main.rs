4f6df682d205c1404fdb3ba9ff39644ec1711143
Nader Tak <nadertak7@gmail.com>
2026-08-06 18:30:21 +0100

    feat: add simple app lifecycle implementation for testing

--- conduit/consumer/src/main.rs
+++ conduit/consumer/src/main.rs
@@ -1,3 +1,6 @@
+use std::time::Duration;
+
+use rumqttc::{Event, Packet};
 use shared::db;
 use shared::logger;
 use shared::settings::Settings;
@@ -11,8 +14,23 @@ async fn main() -> anyhow::Result<()> {
     let settings = Settings::new()?;
     let pool = db::create_connection_pool(&settings.database_settings).await?;
     db::run_migrations(&pool).await?;
-    let (mqtt_client, _event_loop) = mqtt::get_client(&settings.mqtt_settings);
+    let (mqtt_client, mut event_loop) = mqtt::get_client(&settings.mqtt_settings);
     mqtt::subscribe(&mqtt_client, &settings.mqtt_settings).await?;
-    println!("Got to this point.");
-    Ok(())
+
+    println!("Starting to poll");
+    loop {
+        match event_loop.poll().await {
+            Ok(Event::Incoming(Packet::Publish(message))) => {
+                println!(
+                    "Received payload. Topic: {}. Payload: {:?}",
+                    message.topic, message.payload
+                );
+            }
+            Ok(_) => {}
+            Err(e) => {
+                println!("MqttError: {e}");
+                tokio::time::sleep(Duration::from_secs(1)).await;
+            }
+        }
+    }
 }
