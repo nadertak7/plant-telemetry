75db6495affd0a33e96e2af4c0caf245387ab52f
Nader Tak <nadertak7@gmail.com>
2026-08-22 00:25:50 +0100

    test: make db tests DRY

--- conduit/shared/src/db/tests.rs
+++ conduit/shared/src/db/tests.rs
@@ -1,25 +1,42 @@
 use crate::db::MIGRATOR;
 use rstest::rstest;
-use sqlx::{PgPool, postgres::PgQueryResult};
+use sqlx::{Error, PgPool, error::ErrorKind, postgres::PgQueryResult};
 
 enum ExpectedResult {
     Success,
-    ConstraintFailure { constraint: &'static str },
+    ConstraintFailure,
+}
+
+fn validate_database_error<T: std::fmt::Debug>(
+    result: Result<T, Error>,
+    expected_error_kind: ErrorKind,
+    expected_constraint: &str,
+) {
+    let error = result.expect_err("Expected error but insert query succeeded.");
+    let database_error = error
+        .as_database_error()
+        .expect("Expected a database error, got a different error: {database_error}.");
+    let actual_error_kind = database_error.kind();
+    assert_eq!(
+        actual_error_kind, expected_error_kind,
+        "Expected an {expected_error_kind:?} error, but got {actual_error_kind:?}."
+    );
+    assert_eq!(database_error.constraint(), Some(expected_constraint));
 }
 
 #[rstest]
 #[case::happy_path_1(20, 30, 40, ExpectedResult::Success)]
 #[case::happy_path_2(0, 50, 100, ExpectedResult::Success)]
 // Upper threshold out of bounds (greater than 100).
-#[case::adc_perc_above_upper_bound(50, 70, 101, ExpectedResult::ConstraintFailure { constraint: "ck_plant_lower_warning_upper_threshold_perc"})]
+#[case::adc_perc_above_upper_bound(50, 70, 101, ExpectedResult::ConstraintFailure)]
 // Lower threshold out of bounds (less than 0).
-#[case::adc_perc_below_lower_bound(-1, 70, 90, ExpectedResult::ConstraintFailure { constraint: "ck_plant_lower_warning_upper_threshold_perc"})]
+#[case::adc_perc_below_lower_bound(-1, 70, 90, ExpectedResult::ConstraintFailure)]
 // Thresholds in incorrect order - highest to lowest.
-#[case::adc_percs_in_incorrect_order_1(90, 70, 50, ExpectedResult::ConstraintFailure { constraint: "ck_plant_lower_warning_upper_threshold_perc"})]
+#[case::adc_percs_in_incorrect_order_1(90, 70, 50, ExpectedResult::ConstraintFailure)]
 // Thresholds in partially incorrect order - warning adc is lower than lower adc.
-#[case::adc_percs_in_incorrect_order_2(40, 30, 50, ExpectedResult::ConstraintFailure { constraint: "ck_plant_lower_warning_upper_threshold_perc"})]
+#[case::adc_percs_in_incorrect_order_2(40, 30, 50, ExpectedResult::ConstraintFailure)]
 // All thresholds must be unique.
-#[case::adc_percs_must_be_unique(40, 40, 50, ExpectedResult::ConstraintFailure { constraint: "ck_plant_lower_warning_upper_threshold_perc"})]
+#[case::adc_percs_must_be_unique(40, 40, 50, ExpectedResult::ConstraintFailure)]
 #[sqlx::test(migrator = "MIGRATOR")]
 async fn test_lower_warning_upper_adc_thresholds_must_be_sequential(
     #[case] lower_threshold_perc: i16,
@@ -52,24 +69,20 @@ async fn test_lower_warning_upper_adc_thresholds_must_be_sequential(
             assert_eq!(warning_threshold_perc, row.warning_threshold_perc);
             assert_eq!(upper_threshold_perc, row.upper_threshold_perc);
         }
-        ExpectedResult::ConstraintFailure { constraint } => {
-            let error = result.expect_err("Expected error but insert query succeeded.");
-            let database_error = error
-                .as_database_error()
-                .expect("Expected a database error, got a different error: {database_error}.");
-            assert!(
-                database_error.is_check_violation(),
-                "Expected a check violation, got a different error: {database_error}."
+        ExpectedResult::ConstraintFailure => {
+            validate_database_error(
+                result,
+                ErrorKind::CheckViolation,
+                "ck_plant_lower_warning_upper_threshold_perc",
             );
-            assert_eq!(database_error.constraint(), Some(constraint));
         }
     }
 }
 
 #[rstest]
 #[case::happy_path(500, 300, ExpectedResult::Success)]
-#[case::wet_adc_greater_than_dry_adc(300, 500, ExpectedResult::ConstraintFailure { constraint: "ck_sensor_dry_adc_wet_adc" })]
-#[case::wet_adc_equal_to_dry_adc(300, 300, ExpectedResult::ConstraintFailure { constraint: "ck_sensor_dry_adc_wet_adc" })]
+#[case::wet_adc_greater_than_dry_adc(300, 500, ExpectedResult::ConstraintFailure)]
+#[case::wet_adc_equal_to_dry_adc(300, 300, ExpectedResult::ConstraintFailure)]
 #[sqlx::test(migrator = "MIGRATOR")]
 async fn test_dry_adc_must_be_greater_than_wet_adc(
     #[case] dry_adc: i32,
@@ -99,16 +112,12 @@ async fn test_dry_adc_must_be_greater_than_wet_adc(
             assert_eq!(dry_adc, row.dry_adc);
             assert_eq!(wet_adc, row.wet_adc);
         }
-        ExpectedResult::ConstraintFailure { constraint } => {
-            let error = result.expect_err("Expected an error but insert query succeeded.");
-            let database_error = error
-                .as_database_error()
-                .expect("Expected a database error, got a different error: {database_error}");
-            assert!(
-                database_error.is_check_violation(),
-                "Expected a check violation, got a different error: {database_error}."
+        ExpectedResult::ConstraintFailure => {
+            validate_database_error(
+                result,
+                ErrorKind::CheckViolation,
+                "ck_sensor_dry_adc_wet_adc",
             );
-            assert_eq!(database_error.constraint(), Some(constraint));
         }
     }
 }
@@ -144,13 +153,5 @@ async fn test_sensor_and_topic_unique_constraint(pool: PgPool) {
     .execute(&pool)
     .await;
 
-    let error = result.expect_err("Two active sensors with the same topic cannot coexist.");
-    let database_error = error
-        .as_database_error()
-        .expect("Expected database error, got a different error: {database_error}.");
-    assert!(
-        database_error.is_unique_violation(),
-        "Expected a unique violation, got a different error: {database_error}."
-    );
-    assert_eq!(database_error.constraint(), Some("ux_sensor_topic"));
+    validate_database_error(result, ErrorKind::UniqueViolation, "ux_sensor_topic");
 }
